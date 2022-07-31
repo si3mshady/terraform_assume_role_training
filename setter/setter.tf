d0697471db0386dd0531c46ccc21c5a58c18956f
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-07-30 22:16:05 -0500

    save

--- setter/setter.tf
+++ setter/setter.tf
@@ -28,32 +28,85 @@ data "aws_iam_policy" "ec2" {
   #using a managed aws policy for testing. Here we are allowing any entity that uses this policy the ability to have full control over EC2 
 }
 
-data "aws_iam_policy_document" "assume_role" {
-  provider = aws.destination
-  statement {
-    actions = [
-      "sts:AssumeRole",
-      "sts:TagSession",
-      "sts:SetSourceIdentity"
-    ]
-    principals {
-      type        = "AWS"
+# resource "aws_iam_policy" "policy" {
+#   name        = "test-policy"
+#   description = "A test policy"
+
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "sts:AssumeRole"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "*"
+#     }
+#   ]
+# }
+# EOF
+# }
+
+
+
+# data "aws_iam_policy_document" "assume_role" {
+#   provider = aws.destination
+#   statement {
+#     actions = [
+#       "sts:AssumeRole",
+#       "sts:TagSession",
+#       "sts:SetSourceIdentity"
+#     ]
+#     principals {
+#       type        = "AWS"
 
   
-      identifiers = ["arn:aws:iam::698347480743:root"]  #principal being defined is granting access to the SOURCE ACCOUNT to assume a role in the DESTINATION
-      #this api is being called by the DESTINATION ACCOUNT - run this first.  
-    }
-  }
-}
+#       identifiers = ["arn:aws:iam::698347480743:root"]  #principal being defined is granting access to the SOURCE ACCOUNT to assume a role in the DESTINATION
+#       #this api is being called by the DESTINATION ACCOUNT - run this first.  
+#     }
+#   }
+# }
+
+
 
 resource "aws_iam_role" "assume_role" {
-  
-  provider            = aws.destination
-  name                = "assume_role_in_destination_account"
-  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
-  managed_policy_arns = [data.aws_iam_policy.ec2.arn]
-  tags                = {}
+  name = "assume_role_from_source_account"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "AWS": "698347480743"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
+EOF
+}
+
+# resource "aws_iam_role" "assume_role" {
+  
+#   # provider            = aws.destination
+#   name                = "assume_role_in_destination_account"
+
+#   # assume_role_policy  = data.aws_iam_policy_document.assume_role.json
+#   managed_policy_arns = [data.aws_iam_policy.ec2.arn]
+#   tags                = {}
+# }
+
+# resource "aws_iam_policy_attachment" "test-attach" {
+#   name       = "test-attachment"
+#   users      = [aws_iam_user.user.name]
+#   roles      = [aws_iam_role.role.name]
+#   groups     = [aws_iam_group.group.name]
+#   policy_arn = aws_iam_policy.policy.arn
+# }
 
 #this api is being called by the DESTINATION ACCOUNT - run this first.  
 #creates an IAM assume role in the destination account the source account can use! 
